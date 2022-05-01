3cf07253db065fa7092888835803a010747bd17a
Eran Cohen <er11cohen@gmail.com>
2022-05-01 14:26:36 +0300

    Rearrange the code

--- benIshHai/src/main/java/com/eran/benishhai/HumashActivity.java
+++ benIshHai/src/main/java/com/eran/benishhai/HumashActivity.java
@@ -1,87 +1,80 @@
 package com.eran.benishhai;
 
-import com.eran.benishhai.R;
-
-import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-
+import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class HumashActivity extends Activity {
 
-	String yearEn,yearHe;
-	Location location;
+    String yearEn, yearHe;
+    Location location;
 
-	@SuppressLint("NewApi")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_humash);
-		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
-		{
-			ActionBar actionBar = getActionBar();
-			actionBar.setDisplayHomeAsUpEnabled(true);
-		}
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_humash);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
-		Intent intent = getIntent();
-		location = (Location) intent.getParcelableExtra("location");
-		yearHe = location.getYearHe();//intent.getStringExtra("yearHe");
-		setTitle(yearHe);
-		yearEn = location.getYearEn();//intent.getStringExtra("yearEn");
+        Intent intent = getIntent();
+        location = (Location) intent.getParcelableExtra("location");
+        yearHe = location.getYearHe();//intent.getStringExtra("yearHe");
+        setTitle(yearHe);
+        yearEn = location.getYearEn();//intent.getStringExtra("yearEn");
 
-	}
+    }
 
-	public void SelectIntro(View v)
-	{
-		Intent intent = new Intent(getApplicationContext(),WebActivity.class);
+    public void SelectIntro(View v) {
+        Intent intent = new Intent(getApplicationContext(), WebActivity.class);
 //	    	intent.putExtra("yearHe", "בן איש חי");
 //	    	intent.putExtra("yearEn", "intro_1");
 //	    	intent.putExtra("parshHe", "הקדמה");
 //	    	intent.putExtra("parshEn", "intro");
 //	    	intent.putExtra("humashEn", "intro_2");
 
-		location.setYearHe("בן איש חי");
-		location.setYearEn("intro_1");
-		location.setParshHe("הקדמה");
-		location.setParshEn("intro");
-		location.setHumashEn("intro_2");
-		intent.putExtra("location", location);
-		startActivity(intent);
-	}
+        location.setYearHe("בן איש חי");
+        location.setYearEn("intro_1");
+        location.setParshHe("הקדמה");
+        location.setParshEn("intro");
+        location.setHumashEn("intro_2");
+        intent.putExtra("location", location);
+        startActivity(intent);
+    }
 
-	public void SelectHumash(View v)
-	{
-		Intent intent = new Intent(getApplicationContext(),ParashotActivity.class);
+    public void SelectHumash(View v) {
+        Intent intent = new Intent(getApplicationContext(), ParashotActivity.class);
 //    	intent.putExtra("yearHe", yearHe);
 //    	intent.putExtra("yearEn", yearEn);
 //    	String humashEn = (String)((Button)v).getTag();
 //    	intent.putExtra("humashEn", humashEn);
 //    	intent.putExtra("humashHe", ((Button)v).getText());
 
-		location.setHumashEn((String)((Button)v).getTag());
-		location.setHumashHe((String)((Button)v).getText());
-		location.setYearEn(yearEn);
-		location.setYearHe(yearHe);
-		location.setParshHe(null);
-		location.setParshEn(null);
-		intent.putExtra("location", location);
-		startActivity(intent);
+        location.setHumashEn((String) ((Button) v).getTag());
+        location.setHumashHe((String) ((Button) v).getText());
+        location.setYearEn(yearEn);
+        location.setYearHe(yearHe);
+        location.setParshHe(null);
+        location.setParshEn(null);
+        intent.putExtra("location", location);
+        startActivity(intent);
 
-	}
+    }
 
-	public boolean onOptionsItemSelected(MenuItem item){
-		switch (item.getItemId())
-		{
-			case android.R.id.home:
-				finish();
-				break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
--- benIshHai/src/main/java/com/eran/benishhai/SearchInAllBook.java
+++ benIshHai/src/main/java/com/eran/benishhai/SearchInAllBook.java
@@ -1,10 +1,5 @@
 package com.eran.benishhai;
 
-import java.util.ArrayList;
-
-import com.eran.utils.Utils;
-
-import android.R.string;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
@@ -18,203 +13,198 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.SearchView.OnCloseListener;
 import android.widget.SearchView.OnQueryTextListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.eran.utils.Utils;
+
+import java.util.ArrayList;
+
 public class SearchInAllBook extends Activity {
 
-	//android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN/////
-	SearchView searchView;
-	String[] BIHRows = null;
-	String queryText = "";
-	private ListView lv;
-	private ArrayList<Halach> alHalach;
-	ProgressBar progressBar;
-	TextView TVNoResult;
+    //android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN/////
+    SearchView searchView;
+    String[] BIHRows = null;
+    String queryText = "";
+    private ListView lv;
+    private ArrayList<Halach> alHalach;
+    ProgressBar progressBar;
+    TextView TVNoResult;
 
-	@SuppressLint("NewApi")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		ActionBar actionBar = getActionBar();
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		setContentView(R.layout.activity_search_in_all_book);
-		lv = (ListView) findViewById(R.id.ListViewHalach);
-		progressBar = (ProgressBar) findViewById(R.id.progressBarSearchInAllBook);
-		TVNoResult = (TextView) findViewById(R.id.TVNoResult);
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_search_in_all_book);
+        lv = (ListView) findViewById(R.id.ListViewHalach);
+        progressBar = (ProgressBar) findViewById(R.id.progressBarSearchInAllBook);
+        TVNoResult = (TextView) findViewById(R.id.TVNoResult);
 
-		alHalach = new ArrayList<Halach>();
+        alHalach = new ArrayList<Halach>();
 
-		lv.setOnItemClickListener(new OnItemClickListener()
-		{
-			public void onItemClick(AdapterView<?> parent, View currView, int position, long id)
-			{
-				Halach selected = (Halach)lv.getItemAtPosition(position);
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View currView, int position, long id) {
+                Halach selected = (Halach) lv.getItemAtPosition(position);
 
-				Intent intent = new Intent(getApplicationContext(),WebActivity.class);
-				Location location = new Location(selected.getYearEn(), selected.getYearHe() ,selected.getHumashHe(),
-						selected.getHumashEn(),selected.getParshHe(),selected.getParshEn(), queryText);
-				intent.putExtra("location", location);
-				startActivity(intent);
-			}
-		});
+                Intent intent = new Intent(getApplicationContext(), WebActivity.class);
+                Location location = new Location(selected.getYearEn(), selected.getYearHe(), selected.getHumashHe(),
+                        selected.getHumashEn(), selected.getParshHe(), selected.getParshEn(), queryText);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
 
-		String text = Utils.ReadTxtFile("files/benIshHaiCSV.csv", getApplicationContext());
-		BIHRows = text.split("\n");
-	}
+        String text = Utils.ReadTxtFile("files/benIshHaiCSV.csv", getApplicationContext());
+        BIHRows = text.split("\n");
+    }
 
-	@SuppressLint("NewApi")
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.search_in_all_book, menu);
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_in_all_book, menu);
 
 
-		// Toast.makeText(getApplicationContext(),"onQueryTextChange "
-		// ,Toast.LENGTH_LONG).show();
+        // Toast.makeText(getApplicationContext(),"onQueryTextChange "
+        // ,Toast.LENGTH_LONG).show();
 
-		MenuItem searchItem = menu.findItem(R.id.menu_item_search);
-		searchItem.setVisible(true);
-		searchView = (SearchView) searchItem.getActionView();
-		searchView.setQueryHint("חיפוש בכל הספר");
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        searchItem.setVisible(true);
+        searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("חיפוש בכל הספר");
 
-		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
 
-		searchView.setSearchableInfo(searchManager
-				.getSearchableInfo(getComponentName()));
-		searchView.setOnQueryTextListener(new OnQueryTextListener() {
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
 
-			@Override
-			public boolean onQueryTextChange(String query) {
-				//Toast.makeText(getApplicationContext(),"onQueryTextChange " + query ,Toast.LENGTH_LONG).show();
-				int queryLength = query.length();
-				if(queryLength > 0)
-				{
-					String lastChar = query.substring(query.length() - 1);
-					if(lastChar.matches("[a-zA-Z]")){
-						Toast.makeText(getApplicationContext(), "יש להכניס תווים בעברית בלבד" , Toast.LENGTH_SHORT).show();
-						query = query.substring(0, query.length() - 1);//remove last char
-						searchView.setQuery(query, false);
-					}
-				}
-				return true;
-			}
+            @Override
+            public boolean onQueryTextChange(String query) {
+                //Toast.makeText(getApplicationContext(),"onQueryTextChange " + query ,Toast.LENGTH_LONG).show();
+                int queryLength = query.length();
+                if (queryLength > 0) {
+                    String lastChar = query.substring(query.length() - 1);
+                    if (lastChar.matches("[a-zA-Z]")) {
+                        Toast.makeText(getApplicationContext(), "יש להכניס תווים בעברית בלבד", Toast.LENGTH_SHORT).show();
+                        query = query.substring(0, query.length() - 1);//remove last char
+                        searchView.setQuery(query, false);
+                    }
+                }
+                return true;
+            }
 
-			@Override
-			public boolean onQueryTextSubmit(String query) {
-				progressBar.setVisibility(View.VISIBLE);
-				TVNoResult.setVisibility(View.GONE);
-				lv.setVisibility(View.GONE);
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                progressBar.setVisibility(View.VISIBLE);
+                TVNoResult.setVisibility(View.GONE);
+                lv.setVisibility(View.GONE);
 
-				queryText = query;
-				searchView.clearFocus();//close the keyboard
-				searchInAllBook();
-				// TODO Auto-generated method stub
-				return true;
-			}
+                queryText = query;
+                searchView.clearFocus();//close the keyboard
+                searchInAllBook();
+                // TODO Auto-generated method stub
+                return true;
+            }
 
-		});
+        });
 
-		searchView.setOnCloseListener(new OnCloseListener() {
-			@Override
-			public boolean onClose() {
-				return false;
-			}
+        searchView.setOnCloseListener(new OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                return false;
+            }
 
-		});
+        });
 
-		searchView.setOnSearchClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-			}
-		});
+        searchView.setOnSearchClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
 
-		searchView.setIconified(false);//open the search view programmatically
-		return true;
-	}
+        searchView.setIconified(false);//open the search view programmatically
+        return true;
+    }
 
-	@SuppressLint("NewApi")
-	private void closeSearch() {
-		searchView.setIconified(true);// clear the searchView
-		searchView.onActionViewCollapsed();// close the searchView
-	}
+    @SuppressLint("NewApi")
+    private void closeSearch() {
+        searchView.setIconified(true);// clear the searchView
+        searchView.onActionViewCollapsed();// close the searchView
+    }
 
-	@SuppressLint("NewApi")
-	public void onBackPressed() {
+    @SuppressLint("NewApi")
+    public void onBackPressed() {
 
-		if (!searchView.isIconified()) {
-			closeSearch();
-		} else {
-			super.onBackPressed();
-		}
-	}
+        if (!searchView.isIconified()) {
+            closeSearch();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-	// for voice search
-	@SuppressLint("NewApi")
-	@Override
-	protected void onNewIntent(Intent intent) {
-		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-			String query = intent.getStringExtra(SearchManager.QUERY);
-			searchView.setQuery(query, true);
+    // for voice search
+    @SuppressLint("NewApi")
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            searchView.setQuery(query, true);
 
-			// close the keyboard
-			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-			if (imm != null) {
-				imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
-			}
-		}
-	}
+            // close the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+            }
+        }
+    }
 
-	public boolean onOptionsItemSelected(MenuItem item){
-		switch (item.getItemId())
-		{
-			case android.R.id.home:
-				finish();
-				break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	private void searchInAllBook() {
-		try {
-			alHalach.clear();
-			for (int i = 0; i < BIHRows.length; i++)
-			{
-				String[] columns = BIHRows[i].split(",");
-				String parashText = Utils.ReadTxtFile(columns[1] + "/" + columns[4] + "/" + columns[3] + ".html"
-						,getApplicationContext());
-				int count = parashText.split(queryText, -1).length - 1;
-				if(count > 0)
-				{
+    private void searchInAllBook() {
+        try {
+            alHalach.clear();
+            for (int i = 0; i < BIHRows.length; i++) {
+                String[] columns = BIHRows[i].split(",");
+                String parashText = Utils.ReadTxtFile(columns[1] + "/" + columns[4] + "/" + columns[3] + ".html"
+                        , getApplicationContext());
+                int count = parashText.split(queryText, -1).length - 1;
+                if (count > 0) {
 
-					String str = columns[0] + " - " + columns[2] +  " - " + columns[5];
-					str += " - " + count + " תוצאות";
-					Halach halach = new Halach(-2, columns[0], columns[1], columns[2], columns[3], "", columns[4], str, null);
-					alHalach.add(halach);
-				}
-			}
-			ArrayAdapter<Halach> adapter = new ArrayAdapter<Halach>(this,android.R.layout.simple_list_item_1,alHalach);
-			lv.setAdapter(adapter);
+                    String str = columns[0] + " - " + columns[2] + " - " + columns[5];
+                    str += " - " + count + " תוצאות";
+                    Halach halach = new Halach(-2, columns[0], columns[1], columns[2], columns[3], "", columns[4], str, null);
+                    alHalach.add(halach);
+                }
+            }
+            ArrayAdapter<Halach> adapter = new ArrayAdapter<Halach>(this, android.R.layout.simple_list_item_1, alHalach);
+            lv.setAdapter(adapter);
 
-			progressBar.setVisibility(View.GONE);
-			if(alHalach.size() > 0)
-			{
-				lv.setVisibility(View.VISIBLE);
-			}
-			else
-			{
-				TVNoResult.setVisibility(View.VISIBLE);
-			}
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
-	}
+            progressBar.setVisibility(View.GONE);
+            if (alHalach.size() > 0) {
+                lv.setVisibility(View.VISIBLE);
+            } else {
+                TVNoResult.setVisibility(View.VISIBLE);
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
